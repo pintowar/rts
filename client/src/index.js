ce949a5344f52cb10db127cdc630f709df39cee8
Thiago Oliveira <pintowar@gmail.com>
2017-08-22 23:11:33 -0300

    removing remote bootstrap css from index.html

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import About from './modules/About';
 import Monitor from './modules/Monitor';
+import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.css';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
